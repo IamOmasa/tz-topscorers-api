a4287e5755b4645446d2e6aed1d227aef55dd026
Omary Ally <omaryally.em@gmail.com>
2022-07-02 23:42:28 -0400

    heroku host

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.delete('/api/topscorers/:id', (request, response) => {
 })
 
 const PORT = 3000;
-app.listen(PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 });
 
