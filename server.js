8c864dc45b4d9d05870e057edc5248f14bc53bd9
Omary Ally <omaryally.em@gmail.com>
2022-07-05 21:53:41 -0400

    some changes

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ console.log('Its Soccer time!');
 const express = require('express');
 //create express app using function express()
 const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+app.use(cors());
+//to read data from the forminstall body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //database for top scorers
 let topscorers = [
@@ -9,20 +15,25 @@ let topscorers = [
         name: 'mussa mgosi',
         team: 'Simba SC',
         score: '18',
-        id: '2010',
+        year: '2010',
 
     }
 ]
 
+
 //define routes to the application using event handler handling HTTP request 
 app.get('/', (request, response) => {
+
     response.sendFile(__dirname + '/index.html')
 })
+app.post('/topscorers', (request, response) => {
+    console.log(request.body)
+})
 //handle HTTP get reequest made to topScorers
-app.get('/api/topscorers/:id', (request, response) => {
-    const id = request.params.id
-    console.log(id);
-    const topscorer = topscorers.find(topscorer => topscorer.id === id)
+app.get('/api/topscorers/:year', (request, response) => {
+    const year = request.params.year
+    console.log(year);
+    const topscorer = topscorers.find(topscorer => topscorer.year === year)
     if (topscorer) {
         response.json(topscorer)
     } else {
@@ -30,9 +41,9 @@ app.get('/api/topscorers/:id', (request, response) => {
     }
 })
 //Deleting resource
-app.delete('/api/topscorers/:id', (request, response) => {
-    const id = Number(request.params.id)
-    topscorers = topscorers.filter(topscorer => topscorer.id !== id)
+app.delete('/api/topscorers/:year', (request, response) => {
+    const year = Number(request.params.year)
+    topscorers = topscorers.filter(topscorer => topscorer.year !== year)
 
     response.status(204).end()
 })
